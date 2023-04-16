644692c7a8c9afbe8b386844cfaa3b4cf51c4cbe
AmaTsumeAkira <91014219+AmaTsumeAkira@users.noreply.github.com>
2023-04-16 20:16:28 +0800

    Delete akira directory

--- akira/src/main/java/com/akira/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akira.repository;
-
-import com.akira.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- akira/src/main/java/com/akira/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akira.repository;
-
-import com.akira.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
